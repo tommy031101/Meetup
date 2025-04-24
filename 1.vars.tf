132b99c3b84c1e11e821a61c49756279a2c5559d
tommy031101 <tomasz_jakubowski@yahoo.pl>
2025-04-24 15:59:49 +0200

    change variables

---
+++ 1.vars.tf
@@ -0,0 +1,9 @@
+variable "tenant_id" {
+  type    = string
+  default = "5a6b2fb4-b7e6-4d8c-9d10-7301abf0dbcb"
+}
+
+variable "subscription_id" {
+  type    = string
+  default = "174655ab-4346-4b1d-90fb-2dfdeb60e5e8"
+}
